42b882d1b08dc0e1f3743d030425c3a21ce4fe5e
Christian Dinh <Christian Dinh>
2016-01-30 21:25:50 -0600

    Make truck drivable

---
+++ Assets/_Scripts/CarController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+	public WheelCollider FrontLeftWheel;
+	public WheelCollider FrontRightWheel;
+
+	[SerializeField] private float engineTorque;
+	[SerializeField] private float steerCoefficient;
+
+	private bool isOn = false;
+
+	void Update () {
+		if (isOn) {
+			FrontLeftWheel.motorTorque = engineTorque * Input.GetAxis ("Car Accelerate");
+			FrontRightWheel.motorTorque = engineTorque * Input.GetAxis ("Car Accelerate");
+
+			FrontLeftWheel.steerAngle = steerCoefficient * Input.GetAxis ("Strafe");
+			FrontRightWheel.steerAngle = steerCoefficient * Input.GetAxis ("Strafe");
+		}
+	}
+
+	public void TurnOn() {
+		isOn = true;
+	}
+
+	public void TurnOff() {
+		isOn = false;
+	}
+}
---
+++ Assets/_Scripts/EventTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EventTrigger : MonoBehaviour {
+
+	public event Action OnPlayerEnter;
+	public event Action OnPlayerExit;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			OnPlayerEnter ();
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Player") {
+			OnPlayerExit ();
+		}
+	}
+}
