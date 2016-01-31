648471ced8f5bb1bae97ae6c2dafb2c4c91b259f
Christian Dinh <Christian Dinh>
2016-01-30 21:36:13 -0600

    Make this compile

--- Assets/_Scripts/Rain.cs
+++ Assets/_Scripts/Rain.cs
@@ -10,12 +10,12 @@ public class Rain :MonoBehaviour{
 
 	void Start(){
         	while(true){
-             		float percent = Random.value();
+             		float percent = Random.value;
                 	if (percent >= 0.93f){
                     		Debug.Log("It will rain for 2 minutes");
-                    		rain.active = true;
-                    		WaitForSeconds(Random.Range(120.0f, 180.0f));
-            	        	rain.active = false;  
+							rain.emit = true;
+                    		//WaitForSeconds(Random.Range(120, 180));
+							rain.emit = false;  
                 	}
        		 }	
 	}
